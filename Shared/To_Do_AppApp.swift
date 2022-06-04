c8d6acd38fa59668be760adc7e4142da2bbb2595
universal developer <69350489+universal-developer@users.noreply.github.com>
2022-06-04 23:55:29 +0300

    Initial Commit

---
+++ Shared/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  Shared
+//
+//  Created by Artush on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
